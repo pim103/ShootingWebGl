12c2dde9cef5e75a4cab3b0b3840f00b5cbb4d9d
Pim <pim103@hotmail.fr>
2020-06-27 21:58:22 +0200

    Add so****

--- js/camera.js
+++ js/camera.js
@@ -1,12 +1,30 @@
 export class Camera {
     camera;
+    listener;
 
     initCamera() {
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
         this.camera.position.z = 5;
+
+        this.initListener();
     }
 
     getCamera() {
         return this.camera;
+    }
+
+    initListener() {
+        this.listener = new THREE.AudioListener();
+        this.camera.add(this.listener);
+
+        var sound = new THREE.Audio(this.listener);
+
+        const audioLoader = new THREE.AudioLoader();
+        audioLoader.load('../sounds/Yakov_Golman_-_02_-_Forest.mp3', (buffer) => {
+            sound.setBuffer(buffer);
+            sound.setLoop(true);
+            sound.setVolume(0.5);
+            sound.play();
+        });
     }
 }
