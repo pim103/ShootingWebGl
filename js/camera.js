30585dc186aa743726295df7166453232826fac5
lastuck <wolf.hugo7@gmail.com>
2020-06-28 15:39:32 +0200

    added sound

--- js/camera.js
+++ js/camera.js
@@ -21,7 +21,7 @@ export class Camera {
         const sound = new THREE.Audio(this.listener);
 
         const audioLoader = new THREE.AudioLoader();
-        audioLoader.load('../sounds/Yakov_Golman_-_02_-_Forest.mp3', (buffer) => {
+        audioLoader.load('../sounds/The_Elder_Scrolls_Oblivion_Town_Music.mp3', (buffer) => {
             sound.setBuffer(buffer);
             sound.setLoop(true);
             sound.setVolume(0.5);
